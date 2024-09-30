b44cd8ec0ba42748eac34d570767c469810b805d
Dustyn August <daugust@gofundme.com>
2024-09-29 20:56:47 -0700

    Initial Commit

---
+++ Package.swift
@@ -0,0 +1,30 @@
+// swift-tools-version: 5.10
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "PBXProjTool",
+    platforms: [
+        .macOS(.v14),
+    ],
+    dependencies: [
+        .package(
+            url: "https://github.com/apple/swift-argument-parser",
+            from: "1.5.0"
+        ),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package, defining a module or a test suite.
+        // Targets can depend on other targets in this package and products from dependencies.
+        .executableTarget(
+            name: "PBXProjTool",
+            dependencies: [
+                .product(
+                    name: "ArgumentParser",
+                    package: "swift-argument-parser"
+                ),
+            ]
+        ),
+    ]
+)
---
+++ Sources/Tooling/Models/PBXprojSection.swift
@@ -0,0 +1,13 @@
+//
+//  PBXprojSection.swift
+//  
+//
+//  Created by Dustyn August on 9/26/24.
+//
+
+import Foundation
+
+enum PBXprojSection: String, CaseIterable {
+    case PBXNativeTarget
+    case XCConfigurationList
+}
