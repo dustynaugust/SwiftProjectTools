d2d404f0fec01e647e67921f4a0226e029ae052a
Dustyn August <dustyn.august@gmail.com>
2025-05-17 15:28:15 -0700

    Updates output format pattern matching

--- Sources/Tooling/Models/Output/Output+Format.swift
+++ Sources/Tooling/Models/Output/Output+Format.swift
@@ -8,20 +8,35 @@
 import ArgumentParser
 
 extension Output{
- enum Format: String {
-    case text
-    case json = "JSON"
-
-    static var helpDescription: String {
-        allCases.map(\.rawValue).joined(separator: ", ")
+    enum Format: String {
+        case text
+        case json = "JSON"
+        
+        static var helpDescription: String {
+            allCases.map(\.rawValue).joined(separator: ", ")
+        }
     }
-}
 }
 
 extension Output.Format: CaseIterable { }
 
-extension Output.Format: ExpressibleByArgument { }
-
 extension Output.Format: CustomStringConvertible {
     var description: String { rawValue }
+}
+
+extension Output.Format: ExpressibleByArgument {
+    init?(
+        argument: String
+    ) {
+        switch argument.uppercased() {
+        case Output.Format.text.rawValue.uppercased():
+            self = .text
+            
+        case Output.Format.json.rawValue:
+            self = .json
+            
+        default:
+            return nil
+        }
+    }
 }
